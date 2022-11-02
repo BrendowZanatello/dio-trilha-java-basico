01a0bc7448e585e49a23f61ec2d002f34935c88d
BrendowZanatello <brendow_zanatello@hotmail.com>
2022-11-02 12:20:13 -0300

    SmarTv-Metodos

---
+++ Sistema-Smart-TV/src/App.java
@@ -0,0 +1,29 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        SmarTv smarTv = new SmarTv();
+
+        smarTv.diminuirVolume();
+        smarTv.diminuirVolume();
+        smarTv.diminuirVolume();
+        smarTv.aumentarVolume();
+        
+        System.out.println("Canal Atual :" + smarTv.canal);
+
+        smarTv.mudarCanal(13);
+        
+        System.out.println("Canal Atual :" + smarTv.canal);
+
+        System.out.println("Volume Atual :" + smarTv.volume);
+
+        System.out.println("TV Ligada?" + smarTv.ligada);
+        System.out.println("Canal Atual :" + smarTv.canal);
+        System.out.println("Volume Atual :" + smarTv.volume);
+
+        smarTv.ligar ();
+        System.out.println("Novo Status -> TV Ligada?" + smarTv.ligada);
+
+        smarTv.desligar ();
+        System.out.println("Novo Status -> TV Ligada?" + smarTv.ligada);
+    }
+}
